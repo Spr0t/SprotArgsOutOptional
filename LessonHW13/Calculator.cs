4ffad94497b488120a4b5dca918a4c057dec1e75
Dell <Dell@DESKTOP-B6FMJUN>
2022-03-25 13:50:42 +0200

    Добавьте файлы проекта.

---
+++ LessonHW13/Calculator.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace LessonHW13
+{
+    internal class Calculator
+    {
+        public double Average(int[] numbers)
+        {
+
+            double sum = 0;
+
+            foreach (int n in numbers)
+            {
+                sum += n;
+            }
+            return sum / numbers.Length;
+
+        }
+
+        public double Avarage2(params int[] numbers1)
+        {
+            double sum1 = 0;
+
+            foreach (int n in numbers1)
+            {
+                sum1 += n;
+            }
+            return sum1 / numbers1.Length;
+
+        }
+
+        public double CalcTriangleSquare(int ab, int bc, int ac)
+        {
+
+            double p = (ab + bc + ac)/2;
+
+            double square = Math.Sqrt(p*(p - ab)*(p - bc)*(p - ac));
+
+            return square;
+
+        }
+
+        public bool TryDivide(double divisible, double divisor, out double result)
+        {
+            result = 0;
+            if (divisor == 0)
+            {
+                return false;
+            }
+            result = divisible / divisor;
+            return true;
+        }
+        
+        public double CalcTriangleSquare2 (double ab,double ac, int alpha, bool isInRadiance = false)
+        {
+            if(isInRadiance)
+            {
+                return 0.5 * ab * ac * Math.Sin(alpha);
+            }
+            else
+            {
+                double rads = alpha * Math.PI / 180;
+                return 0.5 * ab * ac * Math.Sin(rads);
+            }
+
+
+
+        }
+
+
+
+
+    }
+
+
+
+
+}
---
+++ LessonHW13/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace LessonHW13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculator calc = new Calculator();
+
+            //double avg = calc.Average(new int[] { 1, 2, 3, 4 });
+            //Console.WriteLine($"AVG = {avg}");
+           
+
+            //Calculator calc2 = new Calculator();
+
+            //double avg2 = calc2.Avarage2(1, 2, 3, 4);
+            //Console.WriteLine($"AVG2 = {avg2}");
+            //Console.ReadLine();
+
+
+
+            //Calculator calc = new Calculator();
+
+            //double result = calc.CalcTriangleSquare(ab: 10, bc: 10, ac: 10); // именуем аргументы
+            //Console.WriteLine($"Square = {result}");
+            //Console.ReadLine();
+
+
+            //Calculator divide = new Calculator();             // задаём out - параметр
+            //if(divide.TryDivide(10, 10, out double result))
+            //{
+            //    Console.WriteLine(result);
+            //}    
+            //else
+            //{
+            //    Console.WriteLine("Failde to divide");
+            //}
+            
+            //Console.ReadLine();
+
+            // опциональный параметр 
+
+            //Calculator square = new Calculator();
+            //double result = square.CalcTriangleSquare2(10, 20, 30,true);
+            //Console.WriteLine($"Res = {result}");
+            //Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+        }
+
+        
+    }
+}
